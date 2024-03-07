3d506702f57a9e87c3284ef0ccccc1cf27601b0d
WGrav <63617338+WGrav01@users.noreply.github.com>
2024-03-07 12:44:57 -0500

    more comments

--- utils/bot.py
+++ utils/bot.py
@@ -1,20 +1,21 @@
-import discord
+import discord  # pycord, not discord.py
 import aiosqlite
 import logger
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-log = logger.Logger()
+load_dotenv()  # load the .env file
+
+log = logger.Logger()  # initialize the logger
 
 
-class Bot(discord.AutoShardedBot):
-    async def on_ready(self, owner_id=os.getenv('OWNER_ID')):
+class Bot(discord.AutoShardedBot):  # autosharded bot ensures improved performance when in many servers
+    async def on_ready(self, owner_id=os.getenv('OWNER_ID')):  # runs when the bot logs in (initializes database)
         log.info(f'Logged in successfully as {self.user} with ID {self.user.id}')
-        log.debug(f'Owner ID: {owner_id}')
-        db_init_query = ("CREATE TABLE IF NOT EXISTS Afk"
+        log.debug(f'Owner ID: {owner_id}')  # print the owner ID for debugging
+        db_init_query = ("CREATE TABLE IF NOT EXISTS Afk"  # initalizes the database
                          " (usr INTEGER PRIMARY KEY, status VARCHAR(1024), time_back VARCHAR(1024))")
-        db = aiosqlite.connect('sqlite+aiosqlite:///users.sqlite')
-        await db.execute(db_init_query)
-        await db.commit()
-        await db.close()
+        db = aiosqlite.connect('sqlite+aiosqlite:///users.sqlite')  # connect to the database (and create it if missing)
+        await db.execute(db_init_query)  # execute the query
+        await db.commit()  # commit the changes
+        await db.close()  # close the database
