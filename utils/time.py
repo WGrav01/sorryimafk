e027745e8d5044bc3ca3ec92131f9ed28f5aed6d
WGrav <63617338+WGrav01@users.noreply.github.com>
2024-03-08 10:28:18 -0500

    time.py debug logging

--- utils/time.py
+++ utils/time.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 import pytz
+from utils import logger
+
+log = logger.Logger.afkbot_logger
 
 """
 get the full, formatted, datetime string
@@ -7,5 +10,8 @@ get the full, formatted, datetime string
 
 
 def now():  # get the full datetime string
+    log.debug(f'Now: {datetime.now()}')
     raw_now = datetime.now(pytz.utc)  # get current datetime in utc
+    log.debug(f'Raw now: {raw_now}')
+    log.debug(f'Formatted now: {raw_now.strftime("%d %B, %Y")}')
     return raw_now.strftime('%d %B, %Y')  # convert to string and return
